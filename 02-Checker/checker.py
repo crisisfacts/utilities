d98d5156b18f34d09373ac7678928f09c4e30462
Ian Soboroff <isoboroff@gmail.com>
2023-08-18 18:21:50 -0400

    checker: more comprehensive foramt checks

--- 02-Checker/checker.py
+++ 02-Checker/checker.py
@@ -91,23 +91,30 @@ with open(in_file_path, "r") as in_file:
 for line_num, element in enumerate(checkable_data):
 
     # String types
+    assert "requestID" in element, "ERROR, Line [%d]: requestID not in element" % line_num
+    assert "factText" in element, "ERROR, Line [%d]: factText not in element" % line_num
     assert type(element["requestID"]) == str, "ERROR, Line [%d]: requestID type is not str" % line_num
     assert type(element["factText"]) == str, "ERROR, Line [%d]: factText type is not str" % line_num
 
+    assert "streamID" in element
     assert "streamID" in element, "ERROR, Line [%d]: streamID  not in element" % line_num
     if element["streamID"] is not None:
         assert type(element["streamID"]) == str, "ERROR, Line [%d]: requestID type is not str" % line_num
 
     # int types
+    assert "unixTimestamp" in element, "ERROR, Line [%d]: unixTimestamp not in element" % line_num
     assert type(element["unixTimestamp"]) == int, "ERROR, Line [%d]: unixTimestamp type is not int" % line_num
 
     # float types
+    assert "importance" in element, "ERROR, Line [%d]: importance not in element" % line_num
     assert type(element["importance"]) == float, "ERROR, Line [%d]: importance type is not float" % line_num
 
     # List types
+    assert "sources" in element, "ERROR, Line [%d]: sources not in element" % line_num
     assert type(element["sources"]) == list, "ERROR, Line [%d]: sources type is not list" % line_num
     assert len(element["sources"]) > 0, "ERROR, Line [%d]: sources must be non-empty" % line_num
 
+    assert "informationNeeds" in element, "ERROR, Line [%d]: informationNeeds not in element" % line_num
     if element["informationNeeds"] is not None:
         assert type(element["informationNeeds"]) == list, "ERROR, Line [%d]: informationNeeds type is not list" % line_num
 
